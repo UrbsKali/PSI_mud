7c171de07717f1beb2e3c21bbd82ed82f74da7c7
Tassanix <jjuillet08@gmail.com>
2024-05-02 21:04:17 +0200

    Ajout des flous, detection contours, montrer image cachée dans Scrat_GUI

--- Scrat_GUI/Form1.Designer.cs
+++ Scrat_GUI/Form1.Designer.cs
@@ -42,6 +42,10 @@
             pb_histo = new PictureBox();
             bttn_save = new Button();
             bttn_montrer = new Button();
+            bttn_flou = new Button();
+            button1 = new Button();
+            button8 = new Button();
+            button10 = new Button();
             ((System.ComponentModel.ISupportInitialize)pb_bmp).BeginInit();
             ((System.ComponentModel.ISupportInitialize)nd_rotate).BeginInit();
             ((System.ComponentModel.ISupportInitialize)nd_scale).BeginInit();
@@ -50,7 +54,7 @@
             // 
             // bttn_show
             // 
-            bttn_show.Location = new Point(557, 108);
+            bttn_show.Location = new Point(727, 119);
             bttn_show.Margin = new Padding(3, 2, 3, 2);
             bttn_show.Name = "bttn_show";
             bttn_show.Size = new Size(102, 22);
@@ -61,10 +65,10 @@
             // 
             // pb_bmp
             // 
-            pb_bmp.Location = new Point(12, 11);
+            pb_bmp.Location = new Point(12, 41);
             pb_bmp.Margin = new Padding(3, 2, 3, 2);
             pb_bmp.Name = "pb_bmp";
-            pb_bmp.Size = new Size(471, 284);
+            pb_bmp.Size = new Size(670, 376);
             pb_bmp.SizeMode = PictureBoxSizeMode.CenterImage;
             pb_bmp.TabIndex = 1;
             pb_bmp.TabStop = false;
@@ -72,7 +76,7 @@
             // 
             // button2
             // 
-            button2.Location = new Point(595, 189);
+            button2.Location = new Point(785, 200);
             button2.Margin = new Padding(3, 2, 3, 2);
             button2.Name = "button2";
             button2.Size = new Size(82, 22);
@@ -83,7 +87,7 @@
             // 
             // button3
             // 
-            button3.Location = new Point(595, 161);
+            button3.Location = new Point(785, 172);
             button3.Name = "button3";
             button3.Size = new Size(82, 23);
             button3.TabIndex = 3;
@@ -93,7 +97,7 @@
             // 
             // nd_rotate
             // 
-            nd_rotate.Location = new Point(595, 135);
+            nd_rotate.Location = new Point(785, 146);
             nd_rotate.Maximum = new decimal(new int[] { 180, 0, 0, 0 });
             nd_rotate.Name = "nd_rotate";
             nd_rotate.Size = new Size(82, 23);
@@ -101,7 +105,7 @@
             // 
             // button4
             // 
-            button4.Location = new Point(507, 188);
+            button4.Location = new Point(697, 199);
             button4.Name = "button4";
             button4.Size = new Size(82, 23);
             button4.TabIndex = 6;
@@ -111,7 +115,7 @@
             // 
             // button5
             // 
-            button5.Location = new Point(507, 217);
+            button5.Location = new Point(697, 228);
             button5.Name = "button5";
             button5.Size = new Size(82, 23);
             button5.TabIndex = 7;
@@ -121,7 +125,7 @@
             // 
             // button6
             // 
-            button6.Location = new Point(595, 219);
+            button6.Location = new Point(785, 230);
             button6.Name = "button6";
             button6.Size = new Size(82, 23);
             button6.TabIndex = 8;
@@ -131,7 +135,7 @@
             // 
             // button7
             // 
-            button7.Location = new Point(507, 246);
+            button7.Location = new Point(697, 357);
             button7.Name = "button7";
             button7.Size = new Size(82, 23);
             button7.TabIndex = 9;
@@ -141,7 +145,7 @@
             // 
             // button9
             // 
-            button9.Location = new Point(507, 161);
+            button9.Location = new Point(697, 172);
             button9.Name = "button9";
             button9.Size = new Size(82, 23);
             button9.TabIndex = 11;
@@ -151,7 +155,7 @@
             // 
             // nd_scale
             // 
-            nd_scale.Location = new Point(507, 135);
+            nd_scale.Location = new Point(697, 146);
             nd_scale.Maximum = new decimal(new int[] { 180, 0, 0, 0 });
             nd_scale.Name = "nd_scale";
             nd_scale.Size = new Size(82, 23);
@@ -159,7 +163,7 @@
             // 
             // pb_histo
             // 
-            pb_histo.Location = new Point(510, 12);
+            pb_histo.Location = new Point(700, 23);
             pb_histo.Name = "pb_histo";
             pb_histo.Size = new Size(170, 91);
             pb_histo.TabIndex = 13;
@@ -167,9 +171,9 @@
             // 
             // bttn_save
             // 
-            bttn_save.Location = new Point(557, 287);
+            bttn_save.Location = new Point(727, 417);
             bttn_save.Name = "bttn_save";
-            bttn_save.Size = new Size(82, 23);
+            bttn_save.Size = new Size(102, 23);
             bttn_save.TabIndex = 14;
             bttn_save.Text = "Sauvegarder";
             bttn_save.UseVisualStyleBackColor = true;
@@ -177,7 +181,7 @@
             // 
             // bttn_montrer
             // 
-            bttn_montrer.Location = new Point(595, 246);
+            bttn_montrer.Location = new Point(785, 357);
             bttn_montrer.Name = "bttn_montrer";
             bttn_montrer.Size = new Size(82, 23);
             bttn_montrer.TabIndex = 15;
@@ -185,11 +189,55 @@
             bttn_montrer.UseVisualStyleBackColor = true;
             bttn_montrer.Click += bttn_montrer_Click;
             // 
+            // bttn_flou
+            // 
+            bttn_flou.Location = new Point(697, 276);
+            bttn_flou.Name = "bttn_flou";
+            bttn_flou.Size = new Size(82, 23);
+            bttn_flou.TabIndex = 16;
+            bttn_flou.Text = "BoxBlur";
+            bttn_flou.UseVisualStyleBackColor = true;
+            bttn_flou.Click += bttn_flou_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(785, 276);
+            button1.Name = "button1";
+            button1.Size = new Size(82, 23);
+            button1.TabIndex = 17;
+            button1.Text = "Gauss5x5";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_2;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(697, 305);
+            button8.Name = "button8";
+            button8.Size = new Size(82, 23);
+            button8.TabIndex = 18;
+            button8.Text = "Gauss3x3";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(785, 305);
+            button10.Name = "button10";
+            button10.Size = new Size(82, 23);
+            button10.TabIndex = 19;
+            button10.Text = "Detection";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(700, 338);
+            ClientSize = new Size(895, 472);
+            Controls.Add(button10);
+            Controls.Add(button8);
+            Controls.Add(button1);
+            Controls.Add(bttn_flou);
             Controls.Add(bttn_montrer);
             Controls.Add(bttn_save);
             Controls.Add(pb_histo);
@@ -231,5 +279,9 @@
         private PictureBox pb_histo;
         private Button bttn_save;
         private Button bttn_montrer;
+        private Button bttn_flou;
+        private Button button1;
+        private Button button8;
+        private Button button10;
     }
 }
--- Scrat_GUI/Form1.cs
+++ Scrat_GUI/Form1.cs
@@ -12,10 +12,8 @@ namespace Oscour_GUI
             InitializeComponent();
         }
 
-        string path = ""; // Path to the image 
-        string path2 = ""; // Path to the image
-        int nb = 0; // Number of the image
-        private void button1_Click(object sender, EventArgs e)
+        string path = ""; // chemin de l'image à traiter
+        private void button1_Click(object sender, EventArgs e) // ouvrir une image
         {
             using (OpenFileDialog dlg = new OpenFileDialog())
             {
@@ -26,8 +24,6 @@ namespace Oscour_GUI
                 {
                     path = dlg.FileName;
                     pb_bmp.Image = new Bitmap(path);
-
-                    // Add the new control to its parent's controls collection
                     this.Controls.Add(pb_bmp);
                 }
             }
@@ -44,82 +40,72 @@ namespace Oscour_GUI
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e) // filtre gris
         {
             pb_bmp.Image = null;
             MyImage tmp = new MyImage(path);
             MyImage res = tmp.Greyscale();
             res.Save("output.bmp");
             path = "output.bmp";
-            // Add the new control to its parent's controls collection
-
             pb_bmp.Image = new Bitmap(path);
             this.Controls.Add(pb_bmp);
             histo();
             histo();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e) // rotation selon un angle donné
         {
             pb_bmp.Image = null;
             MyImage tmp = new MyImage(path);
             MyImage res = tmp.Rotate(Convert.ToInt32(Math.Round(nd_rotate.Value, 0)));
             res.Save("output.bmp");
             path = "output.bmp";
-            // Add the new control to its parent's controls collection
-
             pb_bmp.Image = new Bitmap(path);
             this.Controls.Add(pb_bmp);
             histo();
             histo();
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e) // noir et blanc
         {
             pb_bmp.Image = null;
             MyImage tmp = new MyImage(path);
             MyImage res = tmp.BlackAndWhite();
             res.Save("output.bmp");
             path = "output.bmp";
-            // Add the new control to its parent's controls collection
-
             pb_bmp.Image = new Bitmap(path);
             this.Controls.Add(pb_bmp);
             histo();
             histo();
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        private void button5_Click(object sender, EventArgs e) // filtre négatif
         {
             pb_bmp.Image = null;
             MyImage tmp = new MyImage(path);
             MyImage res = tmp.Negative();
             res.Save("output.bmp");
             path = "output.bmp";
-            // Add the new control to its parent's controls collection
-
             pb_bmp.Image = new Bitmap(path);
             this.Controls.Add(pb_bmp);
             histo();
             histo();
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        private void button6_Click(object sender, EventArgs e) // inversion des couleurs
         {
             pb_bmp.Image = null;
             MyImage tmp = new MyImage(path);
             MyImage res = tmp.Invert();
             res.Save("output.bmp");
             path = "output.bmp";
-            // Add the new control to its parent's controls collection
-
             pb_bmp.Image = new Bitmap(path);
             this.Controls.Add(pb_bmp);
             histo();
             histo();
         }
 
-        private void button7_Click(object sender, EventArgs e)
+        private void button7_Click(object sender, EventArgs e) // cacher une image dans une autre
         {
             pb_bmp.Image = null;
             MyImage tmp = new MyImage(path);
@@ -133,8 +119,6 @@ namespace Oscour_GUI
                 {
                     path2 = dlg.FileName;
                     pb_bmp.Image = new Bitmap(path);
-
-                    // Add the new control to its parent's controls collection
                     this.Controls.Add(pb_bmp);
                 }
             }
@@ -142,42 +126,36 @@ namespace Oscour_GUI
             MyImage res = tmp.HideImageInside(tmp2);
             res.Save("output.bmp");
             path = "output.bmp";
-            // Add the new control to its parent's controls collection
-
             pb_bmp.Image = new Bitmap(path);
             this.Controls.Add(pb_bmp);
             histo();
             histo();
         }
 
-        private void histo()
+        private void histo() //generer l'histogramme de l'image
         {
             pb_histo.Image = null;
             MyImage tmp = new MyImage(path);
             MyImage res = tmp.Histogram();
             res.Save("histo.bmp");
-            // Add the new control to its parent's controls collection
-
             pb_histo.Image = new Bitmap("histo.bmp");
             this.Controls.Add(pb_histo);
         }
 
-        private void button9_Click(object sender, EventArgs e)
+        private void button9_Click(object sender, EventArgs e) //Permet de zoomer dans l'image
         {
             pb_bmp.Image = null;
             MyImage tmp = new MyImage(path);
             MyImage res = tmp.Scale(Convert.ToInt32(Math.Round(nd_scale.Value, 0)));
             res.Save("output.bmp");
             path = "output.bmp";
-            // Add the new control to its parent's controls collection
-
             pb_bmp.Image = new Bitmap(path);
             this.Controls.Add(pb_bmp);
             histo();
             histo();
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e) //Permet de sauvegarder l'image dans un dossier spécifié
         {
             CommonOpenFileDialog dialog = new CommonOpenFileDialog();
             dialog.InitialDirectory = "C:\\Users";
@@ -191,14 +169,69 @@ namespace Oscour_GUI
 
         }
 
-        private void bttn_montrer_Click(object sender, EventArgs e)
+        private void bttn_montrer_Click(object sender, EventArgs e) //Permet de montrer l'image cachée dans une autre image
         {
             pb_bmp.Image = null;
             MyImage tmp = new MyImage(path);
             MyImage res = tmp.GetHiddenImage();
             res.Save("output.bmp");
             path = "output.bmp";
-            // Add the new control to its parent's controls collection
+            pb_bmp.Image = new Bitmap(path);
+            this.Controls.Add(pb_bmp);
+            histo();
+            histo();
+        }
+
+        private void bttn_flou_Click(object sender, EventArgs e) //Filtre flou BoxBlur
+        {
+            pb_bmp.Image = null;
+            MyImage tmp = new MyImage(path);
+            MyImage res = tmp.ApplyKernel(Convolution.Kernel.BoxBlur);
+            res.Save("output.bmp");
+            path = "output.bmp";
+
+            pb_bmp.Image = new Bitmap(path);
+            this.Controls.Add(pb_bmp);
+            histo();
+            histo();
+        }
+
+        private void button1_Click_2(object sender, EventArgs e) //Filtre flou GaussianBlur5x5
+        {
+
+            pb_bmp.Image = null;
+            MyImage tmp = new MyImage(path);
+            MyImage res = tmp.ApplyKernel(Convolution.Kernel.GaussianBlur5x5);
+            res.Save("output.bmp");
+            path = "output.bmp";
+
+            pb_bmp.Image = new Bitmap(path);
+            this.Controls.Add(pb_bmp);
+            histo();
+            histo();
+        }
+
+        private void button8_Click(object sender, EventArgs e) //Filtre flou GaussianBlur3x3
+        {
+            pb_bmp.Image = null;
+            MyImage tmp = new MyImage(path);
+            MyImage res = tmp.ApplyKernel(Convolution.Kernel.GaussianBlur3x3);
+            res.Save("output.bmp");
+            path = "output.bmp";
+
+            pb_bmp.Image = new Bitmap(path);
+            this.Controls.Add(pb_bmp);
+            histo();
+            histo();
+        }
+
+        private void button10_Click(object sender, EventArgs e)  //Filtre de detection de contours
+        {
+            pb_bmp.Image = null;
+            MyImage tmp = new MyImage(path);
+            MyImage res = tmp.ApplyKernel(Convolution.Kernel.EdgeDetection3);
+            res.Save("output.bmp");
+            path = "output.bmp";
 
             pb_bmp.Image = new Bitmap(path);
             this.Controls.Add(pb_bmp);
